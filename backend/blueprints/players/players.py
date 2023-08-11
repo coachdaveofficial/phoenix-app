2455a207c2dfafc302e14c9ea754e8b5be4f6af5
David Falson <david@falson.net>
2023-08-11 09:46:09 -0700

    add login required wrap to protected routes

--- backend/blueprints/players/players.py
+++ backend/blueprints/players/players.py
@@ -1,46 +1,52 @@
-from flask import Blueprint, jsonify, make_response, request
+from functools import wraps
+from flask import Blueprint, jsonify, make_response, request, redirect, flash, g
 from services import PlayerService
 from models import PositionType
+from blueprints.auth.auth import login_required
 
 players_bp = Blueprint("players", __name__)
 
 
-@players_bp.route("/players/", methods=["GET", "POST"])
+@players_bp.route("/players/", methods=["GET"])
 def players():
 
-    if request.method == "GET":
-        # if terms are provided in query string, filter results
-        first_name = request.args.get('first_name')
-        last_name = request.args.get('last_name')
-        position = request.args.get('position')
-        team = request.args.get('team')
+    # if terms are provided in query string, filter results
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    position = request.args.get('position')
+    team = request.args.get('team')
 
-        # If an incorrect position is provided it will throw an error since position type is an enum, try/except block will catch this error
-        try:
-            filtered_players = PlayerService.filter_players(
-                first_name=first_name,
-                last_name=last_name,
-                position=position,
-                team=team
-            )
-        except Exception as e:
-            return make_response({"message": "No results, please check your search query and try again"}, 400)
+    # If an incorrect position is provided it will throw an error since position type is an enum, try/except block will catch this error
+    try:
+        filtered_players = PlayerService.filter_players(
+            first_name=first_name,
+            last_name=last_name,
+            position=position,
+            team=team
+        )
+    except Exception as e:
+        return make_response({"message": "No results, please check your search query and try again"}, 400)
 
-        result = []
-        for p in filtered_players:
-            result.append(PlayerService.jsonify_player(p))
-        # This if else block is to provide a clearer response if a search doesn't produce any results (for example, searching for a name that does not exist in the database)
-        if len(result):
-            return make_response(jsonify(result), 200)
-        else:
-            return make_response({"message": "No results, please check your search query and try again"}, 400)
+    result = []
+    for p in filtered_players:
+        result.append(PlayerService.jsonify_player(p))
+    # This if else block is to provide a clearer response if a search doesn't produce any results 
+    # (for example, searching for a name that does not exist in the database could result in an empty list)
+    if len(result):
+        return make_response(jsonify(result), 200)
+    else:
+        return make_response({"message": "No results, please check your search query and try again"}, 400)
 
-    elif request.method == 'POST':
-        player_data = request.get_json()
-        player = PlayerService.create_player_from_json(player_data)
-        json_player = PlayerService.jsonify_player(player)
-        return make_response(jsonify(json_player), 201)
+    
 
+@players_bp.route('/players/', methods=["POST"])
+@login_required
+def add_player():
+
+    player_data = request.get_json()
+    player = PlayerService.create_player_from_json(player_data)
+    json_player = PlayerService.jsonify_player(player)
+    return make_response(jsonify(json_player), 201)
 
 @players_bp.route("/players/<int:id>/", methods=["GET"])
 def get_player(id):
@@ -53,6 +59,7 @@ def get_player(id):
 
 
 @players_bp.route("/players/<int:id>/", methods=["PUT", "DELETE"])
+@login_required
 def update_or_delete_player(id):
 
     player = PlayerService.get_player_by_id(id)
