fc412a1f5cc2f990c385d3d6fdf1ed2b2bf58ffc
David Falson <david@falson.net>
2023-09-14 13:41:42 -0700

    add grid columns for player stats card content

--- frontend/phoenix-frontend/src/components/MatchCard/PlayerStatsCardContent.js
+++ frontend/phoenix-frontend/src/components/MatchCard/PlayerStatsCardContent.js
@@ -6,52 +6,63 @@ const listItemClass = "mb-2 p-2 bg-gray-100 dark:bg-gray-700 rounded-lg";
 export default function PlayerStatsCardContent({ isActive, allTimeGoalScorers, allTimeAssisters, recentSeasonTopScorers, recentSeasonMostAssists }) {
 
     return isActive && (
-        <div className="p-4 bg-white dark:bg-gray-800 rounded-lg">
-            <h2 className={titleClass}>All-Time Most Goals</h2>
-            <ul className="list-disc pl-4">
-                {allTimeGoalScorers && allTimeGoalScorers.map((player, idx) => (
-                    <li key={idx} className={listItemClass}>
-                        <div className="flex justify-between">
-                            <span>{player.player_name}</span>
-                            <span className="font-semibold">{player.goals} goals</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <h2 className={titleClass}>Current Season Most Goals</h2>
-            <ul className="list-disc pl-4">
-                {recentSeasonTopScorers && recentSeasonTopScorers.map((player, idx) => (
-                    <li key={idx} className={listItemClass}>
-                        <div className="flex justify-between">
-                            <span>{player.player_name}</span>
-                            <span className="font-semibold">{player.goals} goals</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+        <div className="p-4 bg-white dark:bg-gray-800 rounded-lg 
+        grid  xs:grid-cols-1 lg:grid-cols-2 gap-2 p-4 content-start">
+            <div>
+                <h2 className={titleClass}>All-Time Most Goals</h2>
+                <ul className="list-disc pl-4">
+                    {allTimeGoalScorers && allTimeGoalScorers.map((player, idx) => (
+                        <li key={idx} className={listItemClass}>
+                            <div className="flex justify-between">
+                                <span>{player.player_name}</span>
+                                <span className="font-semibold">{player.goals} goals</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
-            <h2 className={titleClass}>All-Time Most Assists</h2>
-            <ul className="list-disc pl-4">
-                {allTimeAssisters && allTimeAssisters.map((player, idx) => (
-                    <li key={idx} className={listItemClass}>
-                        <div className="flex justify-between">
-                            <span>{player.player_name}</span>
-                            <span className="font-semibold">{player.assists} assists</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            <h2 className={titleClass}>Current Season Most Assists</h2>
-            <ul className="list-disc pl-4">
-                {recentSeasonMostAssists && recentSeasonMostAssists.map((player, idx) => (
-                    <li key={idx} className={listItemClass}>
-                        <div className="flex justify-between">
-                            <span>{player.player_name}</span>
-                            <span className="font-semibold">{player.assists} assists</span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <h2 className={titleClass}>Current Season Most Goals</h2>
+                <ul className="list-disc pl-4">
+                    {recentSeasonTopScorers && recentSeasonTopScorers.map((player, idx) => (
+                        <li key={idx} className={listItemClass}>
+                            <div className="flex justify-between">
+                                <span>{player.player_name}</span>
+                                <span className="font-semibold">{player.goals} goals</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <h2 className={titleClass}>All-Time Most Assists</h2>
+                <ul className="list-disc pl-4">
+                    {allTimeAssisters && allTimeAssisters.map((player, idx) => (
+                        <li key={idx} className={listItemClass}>
+                            <div className="flex justify-between">
+                                <span>{player.player_name}</span>
+                                <span className="font-semibold">{player.assists} assists</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h2 className={titleClass}>Current Season Most Assists</h2>
+                <ul className="list-disc pl-4">
+                    {recentSeasonMostAssists && recentSeasonMostAssists.map((player, idx) => (
+                        <li key={idx} className={listItemClass}>
+                            <div className="flex justify-between">
+                                <span>{player.player_name}</span>
+                                <span className="font-semibold">{player.assists} assists</span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
         </div>
     );
 }
