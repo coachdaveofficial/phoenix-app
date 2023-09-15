3881b04ec78b60fef45d22102e77b5cfc9b25a6a
David Falson <david@falson.net>
2023-09-15 11:54:17 -0700

    install react-spinners and include About component on page.js

--- frontend/phoenix-frontend/src/app/page.js
+++ frontend/phoenix-frontend/src/app/page.js
@@ -1,19 +1,30 @@
 "use client"
-import React, { useEffect } from 'react';
-import ScrollSection from '@/components/ScrollSections/ScrollSection';
+import React from 'react';
+import ScrollSectionCards from '@/components/ScrollSections/ScrollSectionCards';
+import ScrollSectionAbout from '@/components/ScrollSections/ScrollSectionAbout';
 import NavBarFunc from '@/components/Navbar/Navbar';
 import MatchCard from '@/components/MatchCard/MatchCard';
+import About from '@/components/About/About';
 
 export default function Homepage() {
   return (
     <>
       <NavBarFunc />
-      <ScrollSection sectionId={'section-one'} bgColor={"blue"}>
-        <MatchCard phoenixTeam={"Open"}/>
-        <MatchCard phoenixTeam={"O30"}/>
-        <MatchCard phoenixTeam={"O40"}/>
-      </ScrollSection>
-      <ScrollSection sectionId={'section-two'} bgColor={"orange"} />
+      <ScrollSectionCards sectionId={'section-one'} bgColor={"blue"}>
+        <MatchCard phoenixTeam={"Open"} />
+        <MatchCard phoenixTeam={"O30"} />
+        <MatchCard phoenixTeam={"O40"} />
+      </ScrollSectionCards>
+      <ScrollSectionAbout sectionId={'section-two'} bgColor={"orange"} >
+        <About 
+        youtubeLink={"https://www.youtube.com/@Phoenix_FC/featured"}
+        instagramLink={"https://www.instagram.com/phoenix_fc_pdx/"}
+        facebookLink={"https://www.facebook.com/Phoenixfc.pdx"}
+        leagueLink={"http://gpsdsoccer.com/"}
+        OASALink={"https://www.oregonadultsoccer.com/"}
+        indoorLink={"https://pdxindoorsoccer.com/"}
+        />
+      </ScrollSectionAbout>
     </>
   )
 }
