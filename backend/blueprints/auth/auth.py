65909950f8f24ba2fc18138bf33b1c3356063ccf
David Falson <david@falson.net>
2023-09-18 21:35:15 -0700

    create @me endpoint

--- backend/blueprints/auth/auth.py
+++ backend/blueprints/auth/auth.py
@@ -2,6 +2,7 @@ from functools import wraps
 from flask import Blueprint, jsonify, make_response, request, flash, redirect, g, session
 from services import UserService
 from models import User
+from flask_cors import cross_origin
 
 auth_bp = Blueprint("auth", __name__)
 CURR_USER_KEY = "curr_user"
@@ -21,9 +22,25 @@ def login_required(view_func):
     @wraps(view_func)
     def wrapped_view(*args, **kwargs):
         if g.user is None:
-            return make_response({"message": "nope"})
+            return make_response({"message": "You are not authorized to access this page"}, 401)
         return view_func(*args, **kwargs)
     return wrapped_view
+
+@auth_bp.route('/@me', methods=["GET"])
+@cross_origin(supports_credentials=True)
+def get_current_user():
+    user_id = session.get(CURR_USER_KEY)
+    print(session)
+
+    if not user_id:
+        return make_response({"message": "You are not authorized to access this page"}, 401)
+    
+    user = User.query.get(user_id)
+    print(user, "test")
+    
+
+    return jsonify({"id": user.id, "username": user.username})
+
 
 @auth_bp.route('/signup/', methods=["POST"])
 def signup():
@@ -36,27 +53,26 @@ def signup():
     user = UserService.signup(username, email, password)
 
     if not user:
-        return make_response(jsonify({"message": "Error creating user. Username or Email already taken"}), 401)
-
+        return make_response(jsonify({"message": "Error creating user. Username or Email already taken"}), 400)
+    
     do_login(user)
     return make_response(jsonify({"message": "Succesfully created new user"}), 201)
 
 
 @auth_bp.route('/login/', methods=["POST"])
+@cross_origin(supports_credentials=True)
 def login():
     login_data = request.get_json()
     username = login_data.get("username")
     password = login_data.get("password")
 
     user = UserService.authenticate(username, password)
-
     if not user:
         return make_response(jsonify({"message": "Invalid username or password. Please try again"}), 401)
-    
     do_login(user)
     return make_response(jsonify({"message": "Login successful"}), 200)
 
-@auth_bp.route('/logout/')
+@auth_bp.route('/logout/', methods=["GET"])
 def logout():
     """Handle logout of user."""
     
