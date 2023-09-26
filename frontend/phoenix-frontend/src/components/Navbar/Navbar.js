a8b061793bdb06f839f81846ed34b7160786943b
David Falson <david@falson.net>
2023-09-25 21:04:43 -0700

    add button to open script form modal on Navbar component

--- frontend/phoenix-frontend/src/components/Navbar/Navbar.js
+++ frontend/phoenix-frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import NavLogo from './NavLogo';
 const logoUrl = '/phoenixfc_logo.jpeg'
 
 
-export default function NavBarFunc() {
+export default function NavBarFunc({ handleModalShow }) {
 
     const [showMobileMenu, setShowMobileMenu] = useState(false);
 
@@ -21,13 +21,19 @@ export default function NavBarFunc() {
 
         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <NavLogo 
-                imgSrc={logoUrl} 
-                text={"Phoenix FC"}
-                altText={"Phoenix FC Logo"}
+                <NavLogo
+                    imgSrc={logoUrl}
+                    text={"Phoenix FC"}
+                    altText={"Phoenix FC Logo"}
                 />
                 <div className="flex md:order-2">
-                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                <span className="sr-only">Open upload data form</span>
+                    <button type="button"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        
+                        onClick={handleModalShow}>
+                        Upload Data
+                    </button>
                     <button onClick={toggleMobileMenu} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
